74ada63de0bcfc79f53bcb63cb8250ff52794144
HermiG <Hermi.Hg@gmail.com>
2025-07-03 11:57:47 -0700

    Set a minimum reserve fee in shops

--- src/shop.c
+++ src/shop.c
@@ -1830,7 +1830,7 @@ static void _examine(_ui_context_ptr context)
 
 static void _reserve_aux(shop_ptr shop, obj_ptr obj)
 {
-    int        cost = _sell_price(shop, MIN(10000, obj_value(obj) / 10));
+    int        cost = MAX(10, _sell_price(shop, MIN(10000, obj_value(obj) / 10)));
     string_ptr s;
     char       c;
     char       name[MAX_NLEN];
